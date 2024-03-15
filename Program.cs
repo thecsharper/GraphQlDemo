77f89dfce9225d45d8d8363bdf523de1d098fd5e
thecsharper <si@thecsharper.com>
2024-03-15 11:07:30 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
+var app = builder.Build();
+
+app.MapGraphQL();
+
+app.Run();
+
+public class Book
+{
+    public string Title { get; set; }
+
+    public Author Author { get; set; }
+}
+
+public class Author
+{
+    public string Name { get; set; }
+}
+
+public class Query
+{
+    public Book GetBook() =>
+        new Book
+        {
+            Title = "C# in depth.",
+            Author = new Author
+            {
+                Name = "Jon Skeet"
+            }
+        };
+}
